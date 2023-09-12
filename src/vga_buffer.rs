24447eedabc061f84b00aec1d4c05500231583d1
hpkz <vijayasharma4949@gmail.com>
2023-09-11 23:05:58 -0700

    working up to "vga-text-mode -- A Global Interface"

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,3 +1,7 @@
+use core::fmt::{Write, Result};
+
+use volatile::Volatile;
+
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
@@ -38,16 +42,87 @@ struct ScreenChar {
 }
 
 const BUFFER_HEIGHT: usize = 25;
-const BUFFER_WIDTH  = 80;
+const BUFFER_WIDTH: usize  = 80;
 
 #[repr(transparent)]
 struct Buffer {
-    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
 pub struct Writer {
     column_position: usize,
     color_code: ColorCode,
-    buffer: &'static mut Buffer.
+    buffer: &'static mut Buffer,
 }
 
+impl Write for Writer {
+    fn write_str(&mut self, s: &str) -> Result {
+         self.write_string(s);
+         Ok(())
+    }
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+                else {
+                    let row = BUFFER_HEIGHT-1;
+                    let col = self.column_position;
+                    self.buffer.chars[row][col].write(ScreenChar {
+                        ascii_character: byte,
+                        color_code: self.color_code,
+                    });
+                    self.column_position += 1;
+                }
+            },
+        }
+    }
+
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                _ => self.write_byte(0xfe), 
+            }
+        }
+    }
+    
+    fn new_line(&mut self) {
+        for i in 1..BUFFER_HEIGHT {
+            for j in 0..BUFFER_WIDTH {
+                let char = self.buffer.chars[i][j].read();
+                self.buffer.chars[i-1][j].write(char);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT-1);
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
+}
+
+
+pub fn print_something() {
+    let mut writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Pink, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+    writer.write_byte(b'H'); 
+    write!(writer, "ello, {}!! You have written to an emulated VGA buffer!", "Vijay").unwrap(); 
+    writer.write_string("öö"); 
+}
